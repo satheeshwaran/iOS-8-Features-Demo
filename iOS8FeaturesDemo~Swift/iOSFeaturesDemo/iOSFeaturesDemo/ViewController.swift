36e2f46acca4051228c74702c7f96dd9753c9d83
Satheeshwaran J <satheeshwaran.j@cognizant.com>
2014-09-15 18:48:01 +0530

    Adding Swift files

---
+++ iOS8FeaturesDemo~Swift/iOSFeaturesDemo/iOSFeaturesDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSFeaturesDemo
+//
+//  Created by Satheeshwaran on 9/15/14.
+//  Copyright (c) 2014 Satheeshwaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
